8cc980043833f5119a0ed24d7ab9effb1538b7eb
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-06-15 13:22:06 +0200

    Implement padded seam stitching

--- src/HeightPixel.gd
+++ src/HeightPixel.gd
@@ -16,24 +16,32 @@ static func collect_pixels_from_neighbor_seam(neighbor_heightmap: Image, offset:
 
 	if offset.x == 0:
 		for x in range(neighbor_heightmap.get_width()):
-			var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x, 0 if offset.y > 0 else neighbor_heightmap.get_height() - 1))
-			collected_heights.append(
-				HeightPixel.create(
-					Vector2i(x, 0 if offset.y < 0 else neighbor_heightmap.get_height() - 1),
-					pixel_color.r))
+			for y in [0, 1]:
+				var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x, y if offset.y > 0 else neighbor_heightmap.get_height() - 1 - y))
+				collected_heights.append(
+					HeightPixel.create(
+						Vector2i(x, y if offset.y < 0 else neighbor_heightmap.get_height() - 1 - y),
+						pixel_color.r))
 	elif offset.y == 0:
 		for y in range(neighbor_heightmap.get_height()):
-			var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(0 if offset.x > 0 else neighbor_heightmap.get_width() - 1, y))
-			collected_heights.append(HeightPixel.create(
-				Vector2i(0 if offset.x < 0 else neighbor_heightmap.get_width() - 1, y),
-				pixel_color.r))
+			for x in [0, 1]:
+				var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x if offset.x > 0 else neighbor_heightmap.get_width() - 1 - x, y))
+				collected_heights.append(HeightPixel.create(
+					Vector2i(x if offset.x < 0 else neighbor_heightmap.get_width() - 1 - x, y),
+					pixel_color.r))
 	elif offset.x != 0 and offset.y != 0:
-		var corner_coords = GeometryUtil.get_corner_coords_from_offset(offset, neighbor_heightmap.get_width() - 1, neighbor_heightmap.get_height() - 1)
-		var pixel_color: Color = neighbor_heightmap.get_pixelv(corner_coords)
-		collected_heights.append(
-			HeightPixel.create(
-				Vector2i(neighbor_heightmap.get_width() - 1 - corner_coords.x, neighbor_heightmap.get_height() - 1 - corner_coords.y),
-				pixel_color.r))
+		for x in [0, 1]:
+			for y in [0, 1]:
+				var corner_coords = GeometryUtil.get_corner_coords_from_offset(offset, neighbor_heightmap.get_width() - 1, neighbor_heightmap.get_height() - 1)
+
+				corner_coords.x = corner_coords.x + (x if offset.x == 0 else -x)
+				corner_coords.y = corner_coords.y + (y if offset.y == 0 else -y)
+
+				var pixel_color: Color = neighbor_heightmap.get_pixelv(corner_coords)
+				collected_heights.append(
+					HeightPixel.create(
+						Vector2i(neighbor_heightmap.get_width() - 1 - corner_coords.x, neighbor_heightmap.get_height() - 1 - corner_coords.y),
+						pixel_color.r))
 	else:
 		print_debug("Unsupported offset for seam stitching: ", offset)
 	

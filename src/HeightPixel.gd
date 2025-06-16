69097d1fe6dc1c9e4983c1cd1691d521fee6021c
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-06-16 19:24:32 +0200

    Fix seam stitching

--- src/HeightPixel.gd
+++ src/HeightPixel.gd
@@ -16,33 +16,38 @@ static func collect_pixels_from_neighbor_seam(neighbor_heightmap: Image, offset:
 
 	if offset.x == 0:
 		for x in range(neighbor_heightmap.get_width()):
-			for y in [0, 1]:
-				var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x, y if offset.y > 0 else neighbor_heightmap.get_height() - 1 - y))
+			for y in [0, 1, 2]:
+				var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x, y if offset.y > 0 else neighbor_heightmap.get_height() - 3 + y))
 				collected_heights.append(
 					HeightPixel.create(
-						Vector2i(x, y if offset.y < 0 else neighbor_heightmap.get_height() - 1 - y),
+						Vector2i(x, y if offset.y < 0 else neighbor_heightmap.get_height() - 3 + y),
 						pixel_color.r))
 	elif offset.y == 0:
 		for y in range(neighbor_heightmap.get_height()):
-			for x in [0, 1]:
-				var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x if offset.x > 0 else neighbor_heightmap.get_width() - 1 - x, y))
+			for x in [0, 1, 2]:
+				var pixel_color: Color = neighbor_heightmap.get_pixelv(Vector2i(x if offset.x > 0 else neighbor_heightmap.get_width() - 3 + x, y))
 				collected_heights.append(HeightPixel.create(
-					Vector2i(x if offset.x < 0 else neighbor_heightmap.get_width() - 1 - x, y),
+					Vector2i(x if offset.x < 0 else neighbor_heightmap.get_width() - 3 + x, y),
 					pixel_color.r))
 	elif offset.x != 0 and offset.y != 0:
-		for x in [0, 1]:
-			for y in [0, 1]:
+		for x in [0, 1, 2]:
+			for y in [0, 1, 2]:
 				var corner_coords = GeometryUtil.get_corner_coords_from_offset(offset, neighbor_heightmap.get_width() - 1, neighbor_heightmap.get_height() - 1)
 
-				corner_coords.x = corner_coords.x + (x if corner_coords.x == 0 else -x)
-				corner_coords.y = corner_coords.y + (y if corner_coords.y == 0 else -y)
+				corner_coords.x = (x if corner_coords.x == 0 else corner_coords.x - x)
+				corner_coords.y = (y if corner_coords.y == 0 else corner_coords.y - y)
 
 				var pixel_color: Color = neighbor_heightmap.get_pixelv(corner_coords)
+
+				var opposing_coords = corner_coords - offset * Vector2i(neighbor_heightmap.get_width() - 3, neighbor_heightmap.get_height() - 3)
+
 				collected_heights.append(
 					HeightPixel.create(
-						Vector2i(neighbor_heightmap.get_width() - 1 - corner_coords.x, neighbor_heightmap.get_height() - 1 - corner_coords.y),
+						opposing_coords,
 						pixel_color.r))
+				
+				print("Mapping ", corner_coords, " to ", opposing_coords)
 	else:
 		print_debug("Unsupported offset for seam stitching: ", offset)
-	
+
 	return collected_heights
